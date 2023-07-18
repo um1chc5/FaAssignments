896a4cdd8b27040891889f6bef381c2773f3f5ac
anvuvuong <anvuvuong05@gmail.com>
2023-07-18 22:10:09 +0700

    first

---
+++ todo/src/App.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import Todos from './components/Todos'
+import { Todo } from './Types/Todo.type'
+import Input from './components/Input'
+import Filter from './components/Filter'
+import Header from './components/Header'
+
+const saveTodosToLS = (todos: Todo[]) => {
+  localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+function App() {
+  const storedTodos = localStorage.getItem('todos')
+  const todosInitial = storedTodos
+    ? JSON.parse(storedTodos)
+    : [
+        { id: 1, content: 'Học React', done: true },
+        { id: 2, content: 'Học Java', done: false },
+        { id: 3, content: 'Học Angular', done: false },
+        { id: 4, content: 'Học .Net', done: true }
+      ]
+
+  const [todos, setTodos] = useState(todosInitial as Todo[])
+  const [input, setInput] = useState('')
+  const [filter, setFilter] = useState<'all' | 'todo' | 'done'>('all')
+
+  const changeTodoCompleteSate = (id: number) => () => {
+    setTodos((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, done: !todo.done }
+        }
+        return todo
+      })
+    })
+  }
+
+  // console.log(todos)
+
+  const inputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value)
+  }
+
+  const todoSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (input !== '') {
+      setTodos((prev) => [...prev, { id: prev.length + 1, content: input, done: false }])
+    }
+    setInput('')
+  }
+
+  const filterSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(event.target.value)
+    const value = event.target.value
+    switch (value) {
+      case 'done':
+        setFilter('done')
+        break
+      case 'todo':
+        setFilter('todo')
+        break
+      default:
+        setFilter('all')
+    }
+  }
+
+  const deleteTodoHandler = (id: number) => () => {
+    setTodos((prev) => {
+      if (prev.length === 1) {
+        return []
+      }
+      return prev
+        .slice(0, id - 1)
+        .concat(prev.slice(id))
+        .map((todo, index) => {
+          return { ...todo, id: index + 1 }
+        })
+    })
+  }
+
+  useEffect(() => {
+    saveTodosToLS(todos)
+  }, [todos])
+
+  return (
+    <div className='App'>
+      <div className='todo-container'>
+        <Header />
+        <Input input={input} inputChange={inputChange} todoSubmit={todoSubmit} />
+        <div className='list-container'>
+          <Filter filterSelection={filterSelection} />
+          <div className='list-content'>
+            <Todos
+              changeTodoCompleteSate={changeTodoCompleteSate}
+              todos={todos}
+              filter={filter}
+              deleteTodoHandler={deleteTodoHandler}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ todo/src/components/Input/Input.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './Input.module.css'
+
+interface InputProps {
+  todoSubmit: (event: React.FormEvent<HTMLFormElement>) => void
+  input: string
+  inputChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+function Input(props: InputProps) {
+  const { todoSubmit, input, inputChange } = props
+  return (
+    <form action='' onSubmit={todoSubmit} style={{ display: 'flex', gap: '0.5rem' }}>
+      <input type='text' value={input} onChange={inputChange} className={styles.input} />
+      <button className={styles.button}>
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512' style={{ width: '16px' }}>
+          <path
+            fill='currentColor'
+            d='M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z'
+          />
+        </svg>
+      </button>
+    </form>
+  )
+}
+
+export default Input
