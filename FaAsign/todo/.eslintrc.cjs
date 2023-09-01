0d8038e8820af5d314cb3f73b323673dcd1de303
anvuvuong <anvuvuong05@gmail.com>
2023-09-02 00:13:13 +0700

    change

---
+++ FaAsign/todo/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['react-app', 'prettier', 'eslint-config-prettier'],
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        semi: false,
+        trailingComma: 'none',
+        tabWidth: 2,
+        endOfLine: 'auto',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true
+      }
+    ]
+  }
+}
